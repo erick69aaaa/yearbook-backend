98084740dc342fb05e0f3342dba62c3244e0b900
erick69aaaa <esb12@aluno.ifnmg.edu.br>
2026-05-12 19:44:01 +0000

    feat: cria servidor Express com ES Modules, vercel.json e rota inicial

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+// rota principal
+app.get('/', (req, res) => {
+  res.json({
+    mensagem: 'Yearbook API está no ar! 🎓'
+  });
+});
+
+// desafio do aluno
+app.get('/status', (req, res) => {
+  res.json({
+    status: 'ok',
+    timestamp: new Date()
+  });
+});
+
+if (process.env.VERCEL !== '1') {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+}
+
+export default app;
